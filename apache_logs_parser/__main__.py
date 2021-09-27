edc8503bec1ce183078ab39cfc2ca2d6eb310223
Martin DENIZET <martin.denizet@supinfo.com>
2021-09-27 23:08:45 +0200

    Implemented entrypoint

--- apache_logs_parser/__main__.py
+++ apache_logs_parser/__main__.py
@@ -5,6 +5,7 @@
 import argparse
 import logging
 from apache_logs_parser import actions
+from apache_logs_parser.parser import write_json
 
 
 def main():
@@ -12,7 +13,8 @@ def main():
     parser.add_argument("-v", "--verbose", help="increase output verbosity",
                         action="store_true")
 
-    parser.add_argument(dest='apache_log_files', type=argparse.FileType('r'), help="Input apache log input_files")
+    parser.add_argument(dest='apache_log_files', type=argparse.FileType('r'), help="Input apache log input_files",
+                        nargs='+')
     parser.add_argument('-o', '--output-json', type=argparse.FileType('w'), help="Output path of the JSON file")
 
     parser.add_argument("-a", "--action", type=str, choices=actions.ACTIONS, default=actions.CONVERT_AND_STATS,
@@ -28,6 +30,12 @@ def process_args(args):
     if args.verbose:
         log_level = logging.DEBUG
     logging.basicConfig(level=log_level)
+
+    data = write_json([f.name for f in args.apache_log_files],
+                      args.output_json.name,
+                      generate_stats=args.action in (actions.CONVERT_AND_STATS, actions.STATS),
+                      include_log_entries=args.action in (actions.CONVERT_AND_STATS, actions.CONVERT),
+                      )
 
 
 if __name__ == '__main__':
